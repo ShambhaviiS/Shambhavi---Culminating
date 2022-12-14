0d62ce5054b654ecc5e98efaa3819f3988047dbd
Shambhavi S - 11TM 746155 - <120031281+ShambhaviiS@users.noreply.github.com>
2022-12-14 01:28:53 +0000

    Testing a moving character

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-import pygame, sys
-from pygame.locals import QUIT
-
+import pygame
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
+
+screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption('Moving character test')
+x = 100
+y = 100
+speed = 3
+width = 10
+height = 7
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
+    pygame.time.delay(10)
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            quit()
+
+    screen.fill('white')
+    key  = pygame.key.get_pressed()
+  
+    if key[pygame.K_LEFT] and x>0:
+      
+        x -= speed
+        print('l')
+    if key[pygame.K_RIGHT] and x<500-width:
+        x += speed
+        print('r')
+    if key[pygame.K_UP] and y>0:
+        y -= speed
+        print('u')
+    if key[pygame.K_DOWN] and y<500-height:
+        y += speed
+        print('d')
+    pygame.draw.rect(screen, 'black',(x,y,width, height))
+    pygame.draw.circle(screen, 'black', (x+5,y-5),5)
     pygame.display.update()
+
+    
