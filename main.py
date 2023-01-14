ec64491655112363da57b85c83ca27f81dff5cfe
Shambhavi S - 11TM 746155 - <120031281+ShambhaviiS@users.noreply.github.com>
2023-01-14 05:41:05 +0000

    collisions

--- main.py
+++ main.py
@@ -28,6 +28,110 @@ grass11_img = pygame.image.load('grass11.png')
 grass12_img = pygame.image.load('grass12.png')
 plant_png = pygame.image.load('plant.png')
 
+class Player():
+  def __init__(self, x, y):
+    self.images_right = []
+    self.images_left = []
+    self.index = 0
+    self.counter = 0
+    for num in range(1, 4):
+      img_right = pygame.image.load('running'+str(num)+'.png')
+      img_right = pygame.transform.scale(img_right, (25, 45))
+      img_left = pygame.transform.flip(img_right, True, False)
+      self.images_right.append(img_right)
+      self.images_left.append(img_left)
+    self.index = 0
+    self.image = self.images_right[self.index]
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.width = self.image.get_width()
+    self.height = self.image.get_height()
+    self.vel_y = 0
+    self.jumped = False
+    self.direction = 0
+    self.dx = 0
+    self.dy = 0
+    self.move_right = False
+    self.move_left = False
+
+  def update(self):
+    dx = 0
+    dy = 0
+    walk_cooldown = 5
+  
+    #get keypresses
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SPACE] and self.jumped == False:
+      self.vel_y = -15
+      self.jumped = True
+    if key[pygame.K_SPACE] == False:
+      self.jumped = False
+    if key[pygame.K_LEFT]:
+      dx -= 2
+      self.counter += 1
+      self.direction = -1
+    if key[pygame.K_RIGHT]:
+      dx += 2
+      self.counter += 1
+      self.direction = 1
+    if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+      self.counter = 0
+      self.index = 0
+      if self.direction == 1:
+        self.image = self.images_right[self.index]
+      if self.direction == -1:
+        self.image = self.images_left[self.index]
+  
+  
+    #handle animation
+    if self.counter > walk_cooldown:
+      self.counter = 0	
+      self.index += 1
+      if self.index >= len(self.images_right):
+        self.index = 0
+      if self.direction == 1:
+        self.image = self.images_right[self.index]
+      if self.direction == -1:
+        self.image = self.images_left[self.index]
+  
+  
+    #add gravity
+    self.vel_y += 1
+    if self.vel_y > 10:
+      self.vel_y = 10
+    dy += self.vel_y
+  
+    #check for collision
+    for tile in world.tile_list:
+      #check for collision in x direction
+      if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+        dx = 0
+      #check for collision in y direction
+      if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+        #check if below the ground i.e. jumping
+        if self.vel_y < 0:
+          dy = tile[1].bottom - self.rect.top
+          self.vel_y = 0
+        #check if above the ground i.e. falling
+        elif self.vel_y >= 0:
+          dy = tile[1].top - self.rect.bottom
+          self.vel_y = 0
+  
+  
+  
+  
+    #update player coordinates
+    self.rect.x += dx
+    self.rect.y += dy
+  
+    if self.rect.bottom > screen_height:
+      self.rect.bottom = screen_height
+      dy = 0
+  
+    #draw player onto screen
+    screen.blit(self.image, self.rect)
+    
 
 class World(): 
   def __init__(self, data):
@@ -155,30 +259,31 @@ class World():
     for tile in self.tile_list:
       screen.blit(tile[0], tile[1])
 
-
 level1_map = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
 [2, 2, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[1, 1, 10, 6, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-[4, 7, 0, 0, 0, 0, 14, 0, 0, 0, 0, 0, 0, 9, 2, 6, 0, 1, 1, 0],
-[0, 0, 0, 14, 0, 9, 2, 6, 0, 0, 0, 0, 9, 13, 0, 0, 0, 1, 1, 0],
+[1, 1, 10, 2, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 2, 6, 0, 1, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 9, 2, 6, 0, 0, 0, 0, 9, 13, 0, 0, 0, 1, 1, 0],
 [0, 0, 9, 6, 0, 5, 1, 3, 0, 0, 0, 0, 5, 1, 0, 0, 1, 1, 1, 0],
 [0, 0, 5, 3, 0, 5, 1, 10, 6, 0, 9, 2, 13, 1, 6, 0, 1, 1, 1, 0],
 [2, 2, 13, 3, 0, 5, 1, 1, 3, 0, 5, 1, 1, 1, 3, 0, 1, 1, 1, 0],
-[1, 1, 1, 3, 0, 5, 1, 1, 3, 0, 5, 1, 1, 1, 3, 0, 1, 1, 1, 0],
-[1, 1, 1, 3, 0, 5, 1, 1, 3, 0, 5, 1, 1, 1, 3, 0, 1, 1, 1, 0],
 [1, 1, 1, 3, 0, 5, 1, 1, 3, 0, 5, 1, 1, 1, 3, 0, 1, 1, 1, 0]]
 
-
 world = World(level1_map)
+player = Player(100, screen_height - 130)
 
 run = True
 while run:
 
-	screen.blit(bg, (0, 0))
-
+	screen.blit(bg_img, (0, 0))
+  
 	world.draw()
+
+	player.update()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
